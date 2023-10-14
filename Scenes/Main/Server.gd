f395b7c0f63cdada3254085f38304b6aebbb7767
SunnyCat10 <daniel.dovgun@gmail.com>
2023-10-14 23:59:53 +0300

    Added server scripts

--- Scenes/Main/Server.gd
+++ Scenes/Main/Server.gd
@@ -1,11 +1,27 @@
 extends Node
 
+var network : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+const PORT : int = 34684
+const MAX_PLAYERS : int = 10
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	start_server() 
 
+func start_server():
+	network.create_server(PORT, MAX_PLAYERS)
+	multiplayer.multiplayer_peer = network
+	print("Server started")
+	
+	multiplayer.peer_connected.connect(_peer_connected)
+	multiplayer.peer_disconnected.connect(_peer_disconnected)
+	
+	print(self.is_multiplayer_authority())
+	
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _peer_connected(player_id):
+	print("User", str(player_id), "Connected")
+	
+func _peer_disconnected(player_id):
+	print("User", str(player_id), "Disconnected")
+	
