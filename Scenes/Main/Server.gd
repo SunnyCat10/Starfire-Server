08ebac48b84409af11d9c0940618e51ff1ab130c
SunnyCat10 <daniel.dovgun@gmail.com>
2023-10-17 00:30:47 +0300

    Added player sync

--- Scenes/Main/Server.gd
+++ Scenes/Main/Server.gd
@@ -4,8 +4,11 @@ var network : ENetMultiplayerPeer = ENetMultiplayerPeer.new()
 const PORT : int = 34684
 const MAX_PLAYERS : int = 10
 
+var player_state_collection = {}
+
 @rpc func spawn_new_player(player_id : int, position : Vector2): pass
 @rpc func despawn_player(player_id: int): pass
+@rpc("unreliable_ordered") func recive_world_state(world_state): pass
 
 
 func _ready():
@@ -26,13 +29,24 @@ func _peer_connected(player_id):
 
 
 func _peer_disconnected(player_id):
-	despawn_player.rpc(player_id)
 	print("User", str(player_id), "Disconnected")
-
+	if has_node(str(player_id)):
+		get_node(str(player_id)).queue_free()
+		player_state_collection.erase(player_id)
+		despawn_player.rpc(player_id)
 
 @rpc("any_peer")
 func get_location(location : Vector2):
 	print(multiplayer.get_remote_sender_id(), " > ", location)
 	
 
-
+@rpc("any_peer", "unreliable_ordered") func recive_player_state(player_state):
+	var player_id : int = multiplayer.get_remote_sender_id()
+	if player_state_collection.has(player_id):
+		if player_state_collection[player_id]["T"] < player_state["T"]:
+			player_state_collection[player_id] = player_state
+	else:
+		player_state_collection[player_id] = player_state
+		
+func send_world_state(world_state):
+	recive_world_state.rpc(world_state)
---
+++ Scenes/Main/StateProcessing.gd
@@ -0,0 +1,18 @@
+extends Node
+
+var world_state
+
+@onready var server : Node = get_parent()
+
+func _physics_process(delta):
+	if not server.player_state_collection.empty():
+		world_state = server.player_state_collection.duplicate(true)
+		for player in world_state.keys():
+			world_state[player].erase("T")
+		world_state["T"] = Time.get_unix_time_from_datetime_string(Time.get_time_string_from_system())
+		# Verification
+		# Anti-Cheat
+		# Cuts ( chunking / maps )
+		# Physics checks
+		# Anything else :D
+		server.send_world_state(world_state)
