4fe8718d2f5c1d216a66d064c3c415dfec870071
Amouxi <FromHearthZero@gmail.com>
2023-10-27 21:41:26 +0300

    Added Server side collision
    Added Server side collision
    server debug shows all players, correct position and rotation.
    Fixed projectile impact on servers.
    
    Co-Authored-By: SunnyCat10 <68126925+SunnyCat10@users.noreply.github.com>

--- Scenes/Projectiles/GerbilProjectile.gd
+++ Scenes/Projectiles/GerbilProjectile.gd
@@ -5,17 +5,24 @@ var speed : float = 200
 var life_time = 1.0
 var impulse_rotation
 var player_id : int
-
+@onready var area2d :Area2D = $Area2D
 
 func _ready():
-	body_entered.connect(on_impact)
+#	body_entered.connect(on_impact)
+	area2d.body_entered.connect(on_impact)
 	apply_central_impulse(Vector2(speed, 0).rotated(global_rotation))
+	
 	await get_tree().create_timer(life_time).timeout.connect(destroy_projectile)
 
 
-func on_impact(body: Node) -> void:
-	destroy_projectile()
-	
+func on_impact(body: Node2D) -> void:
+	if body.name != str(player_id):
+		print(body.name,"was hit by projectile owned by", player_id)
+		if body.is_in_group("players"):
+			print("Hit player") # TODO SEND RPC
+		
+		destroy_projectile()
+
 
 func destroy_projectile():
 	set_deferred("freeze", true)
